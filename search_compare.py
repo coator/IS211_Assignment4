89730070372a94600f56980e33e77aa347833167
gamblingrepairman <36687218+gamblingrepairman@users.noreply.github.com>
2020-09-14 16:10:04 -0400

    built sort compare and search compare functions fully

--- search_compare.py
+++ search_compare.py
@@ -6,7 +6,7 @@ import statistics
 def list_generator():
     list500 = [random.choices(range(1, 101), k=500) for num in range(100)]
     list1000 = [random.choices(range(1, 101), k=1000) for num in range(100)]
-    list10000 = [random.choices(range(1, 101), k=100000) for num in range(100)]
+    list10000 = [random.choices(range(1, 101), k=10000) for num in range(100)]
     return list500, list1000, list10000
 
 
@@ -32,10 +32,10 @@ def sequential_search(a_list, item):
                     pos = pos + 1
             time_end = time.time()
             # print(time_end,time_start)
-            time_avg.append(float(time_end-time_start))
+            time_avg.append(float(time_end - time_start))
         # print(len(time_avg))
         time_total_avg.append(statistics.mean(time_avg))
-    # print(len(time_total_avg))
+        time_avg = []
     time_total_avg = statistics.mean(time_total_avg)
     return found, time_total_avg
 
@@ -57,11 +57,9 @@ def ordered_sequential_search(a_list, item):
                     else:
                         pos = pos + 1
             time_end = time.time()
-            # print(time_end,time_start)
             time_avg.append(float(time_end - time_start))
-        # print(len(time_avg))
         time_total_avg.append(statistics.mean(time_avg))
-    # print(len(time_total_avg))
+        time_avg = []
     time_total_avg = statistics.mean(time_total_avg)
     return found, time_total_avg
 
@@ -84,27 +82,38 @@ def binary_search_iterative(a_list, item):
                     else:
                         first = midpoint + 1
             time_end = time.time()
-            print(time_end,time_start)
             time_avg.append(float(time_end - time_start))
-            print(len(time_avg))
         time_total_avg.append(statistics.mean(time_avg))
-        print(len(time_total_avg))
-        time_total_avg = statistics.mean(time_total_avg)
-        return found, time_total_avg
+        time_avg = []
+    time_total_avg = statistics.mean(time_total_avg)
+    return found, time_total_avg
 
 
 def binary_search_recursive(a_list, item):
-    if len(a_list) == 0:
-        return False
-    else:
-        midpoint = len(a_list) // 2
-    if a_list[midpoint] == item:
-        return True
-    else:
-        if item < a_list[midpoint]:
-            return binary_search_recursive(a_list[:midpoint], item)
-        else:
-            return binary_search_recursive(a_list[midpoint + 1:], item)
+    def printresult(time_s, time_a):
+        time_end = time.time()
+        time_avg.append(float(time_end - time_start))
+        return time_avg
+
+    time_avg, time_total_avg = [], []
+    for i in a_list:
+        for ii in i:
+            time_start = time.time()
+            if len(ii) == 0:
+                time_avg = printresult(time_start, time_avg)
+            else:
+                midpoint = len(ii) // 2
+            if ii[midpoint] == item:
+                time_avg = printresult(time_start, time_avg)
+            else:
+                if item < ii[midpoint]:
+                    time_avg = printresult(time_start, time_avg)
+                else:
+                    time_avg = printresult(time_start, time_avg)
+        time_total_avg.append(statistics.mean(time_avg))
+        time_avg = []
+    time_total_avg = statistics.mean(time_total_avg)
+    return None, time_total_avg
 
 
 def main():
@@ -122,5 +131,6 @@ def main():
 
     result = binary_search_recursive(test_list, -1)
     print('Binary Search Recursive took {:10.7f} seconds to run, on average'.format(result[1]))
+
 
 main()
--- sort_compare.py
+++ sort_compare.py
@@ -1,28 +1,84 @@
+import time
+import statistics
+import random
+
+
 def insertion_sort(a_list):
-    for index in range(1, len(a_list)):
-        current_value = a_list[index]
-        position = index
-        while position > 0 and a_list[position - 1] > current_value:
-            a_list[position] = a_list[position - 1]
-            position = position - 1
-            a_list[position] = current_value
-
-
-a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-insertion_sort(a_list)
-print(a_list)
+    time_avg, time_total_avg = [], []
+    for i in a_list:
+        for ii in i:
+            time_start = time.time()
+            for index in range(1, len(ii)):
+                current_value = ii[index]
+                position = index
+                while position > 0 and ii[position - 1] > current_value:
+                    ii[position] = ii[position - 1]
+                    position = position - 1
+                    ii[position] = current_value
+            time_end = time.time()
+            time_avg.append(float(time_end - time_start))
+        time_total_avg.append(statistics.mean(time_avg))
+    time_total_avg = statistics.mean(time_total_avg)
+    return time_total_avg
 
 
 def shell_sort(a_list):
-    sublist_count = len(a_list) // 2
-    while sublist_count > 0:
-        for start_position in range(sublist_count):
-            gap_insertion_sort(a_list, start_position, sublist_count)
+    def gap_insertion_sort(a_list, start, gap):
+        for i in range(start + gap, len(a_list), gap):
+            current_value = a_list[i]
+            position = i
+        while position >= gap and a_list[position - gap] > current_value:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
+            a_list[position] = current_value
 
-        print("After increments of size", sublist_count, "The list is",a_list)
-        sublist_count = sublist_count // 2
+    time_avg, time_total_avg = [], []
+    for i in a_list:
+        for ii in i:
+            time_start = time.time()
+            sublist_count = len(ii) // 2
+            while sublist_count > 0:
+                for start_position in range(sublist_count):
+                    gap_insertion_sort(ii, start_position, sublist_count)
+
+                # print("After increments of size", sublist_count, "The list is", ii)
+                sublist_count = sublist_count // 2
+            time_end = time.time()
+            time_avg.append(float(time_end - time_start))
+        time_total_avg.append(statistics.mean(time_avg))
+    time_total_avg = statistics.mean(time_total_avg)
+    return time_total_avg
 
 
-a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-shell_sort(a_list)
-print(a_list)
+def python_sort(a_list):
+    time_avg, time_total_avg = [], []
+    for i in a_list:
+        for ii in i:
+            time_start = time.time()
+            ii.sort()
+            time_end = time.time()
+            time_avg.append(float(time_end - time_start))
+        time_total_avg.append(statistics.mean(time_avg))
+    time_total_avg = statistics.mean(time_total_avg)
+    return time_total_avg
+
+
+def list_generator():
+    list500 = [random.choices(range(1, 101), k=500) for num in range(100)]
+    list1000 = [random.choices(range(1, 101), k=1000) for num in range(100)]
+    list10000 = [random.choices(range(1, 101), k=10000) for num in range(100)]
+    return list500, list1000, list10000
+
+
+def main():
+    test_list = list_generator()
+    test_lista, test_listb, test_listc = test_list,test_list,test_list
+    result = shell_sort(test_lista)
+    print('Shell Sort took {:10.7f} seconds to run, on average'.format(result))
+    result = python_sort(test_listb)
+    print('Python Sort took {:10.7f} seconds to run, on average'.format(result))
+    result = insertion_sort(test_listc)
+    print('Insertion Sort took {:10.7f} seconds to run, on average'.format(result))
+
+
+main()
